be9addb2612e44714eb1286b0ab4be8f3b02cb19
Shayokh <shayokhshuvro14@gmail.com>
2025-05-26 21:11:37 +0600

    install contentful and add empty footer

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+	return (
+		<header>
+			<nav>
+				<p className="text-amber-300">Some stuff here</p>
+			</nav>
+		</header>
+	);
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,11 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
-import Header from '../components/Header'
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import Header from "../components/Header";
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Header />
-
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
-})
+	component: () => (
+		<>
+			<Header />
+			<Outlet />
+		</>
+	),
+});
